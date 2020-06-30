5066f85029d6d2addaa2841658464c1a92c0ad51
as <simana.annesophie@gmail.com>
2020-06-30 16:59:09 +0200

    data input

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,41 +1,108 @@
-// async function testDiv() {
-//     const test = await dataConsult();
-//     const template = await document.querySelector("#tpl-test").content;
-//     for (const element of test) {
-//         const clone = template.cloneNode(true);
+//      async function testDiv() {
+//      const test = await dataConsult();
+//      const template = await document.querySelector("#tpl-test").content;
+//      for (const element of test) {
+//          const clone = template.cloneNode(true);
 
-//         clone.querySelector("#name").innerHTML = await element.name;
+//          clone.querySelector("#name").innerHTML = await element.name;
 //         clone.querySelector("#desc").innerHTML = await md.render(element.description);
 
 //         document.querySelector("#target").appendChild(clone);
 //     }
-// }
+//  }
 
 // testDiv();
 
 
 
- function afficher() {
-  let name = document.getElementById("formGroupExampleInput").value;
-  let shortDescrition  = document.getElementById("ormGroupExampleInput2").value;
-  let description = document.getElementById("exampleFormControlTextarea1").value;
-        console.log();
-   
-     }
-   afficher(res);
 
-    // function charUpdate() {
-    //          const charEdit = {
-    //              // TODO: Change params to InnerHTML & check for the image upload
-    //              name:  document.getElementById("formGroupExampleInput").value,
-    //              shortDescription: "Axios rox ou p'tetre pas",
-    //              description: "Juste pour rire",
-    //              image: "",
-    //          };
-    //          return charEdit;
-    //      }
+ let body = document.querySelector('body');
+ console.log(body);
+ 
+
+
+
+  body.addEventListener("click", function (e) {
+   if (e.target.id == "save") {
+    function result(){
+      let name = document.getElementById('formGroupExampleInput').value;
+      let shortDescription = document.getElementById('formGroupExampleInput2').value;
+      let description = document.getElementById('exampleFormControlTextarea1').value;
+
+
+function readerHandler(e2) 
+{ 
+  var store = document.getElementById('exampleFormControlFile1');
+  store.innerHTML=e2.target.result.split("\n").join("<br>"); 
+}
+
+function readfile(e1)
+{
+  var fileobj = e1.target.files[0]; 
+  var fr = new FileReader();
+  fr.onload = readerHandler;  
+  fr.readAsText(fileobj); 
+}
+
+window.onload=function()
+{
+  var x = document.getElementById("filebrowsed");
+  x.addEventListener('change', readfile, false);
+}
+
+
+
+
+
+
+
+
+            // function loadImage(url) {
+//       return new Promise((resolve, reject) => {
+//       let img = new Image();
+//       img.addEventListener('load', e => resolve(img));
+//       img.addEventListener('error', () => {
+//       reject(new Error(`Failed to load image's URL: ${url}`));
+//     });
+//     img.src = url;
+//   });
+// }
+
+// loadImage('https://i.imgur.com/3tU4Vig.jpg')
+//   .then(img => document.getElementById('image-holder').appendChild(img))
+//   .catch(error => console.error(error));
     
+//     }
+
+      //  sessionStorage.setItem("nom", name);
+      //  sessionStorage.setItem("shortDescription",shortDescription);
+      //  sessionStrorage.setItem("description", description);
+      
+//       function loadImage(url) {
+//       return new Promise((resolve, reject) => {
+//       let img = new Image();
+//       img.addEventListener('load', e => resolve(img));
+//       img.addEventListener('error', () => {
+//       reject(new Error(`Failed to load image's URL: ${url}`));
+//     });
+//     img.src = url;
+//   });
+// }
+
+// loadImage('https://i.imgur.com/3tU4Vig.jpg')
+//   .then(img => document.getElementById('image-holder').appendChild(img))
+//   .catch(error => console.error(error));
+    
+//     }
+      
+  
        
+
+      
+ }
+ 
+//  });
+
 
 
 
