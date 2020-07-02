0f6bec6ef15fa26f71ee680d23f3834d6d4b1b95
as <simana.annesophie@gmail.com>
2020-07-02 11:33:14 +0200

    recuperer img test

--- assets/js/index.js
+++ assets/js/index.js
@@ -24,31 +24,50 @@
 
   body.addEventListener("click", function (e) {
    if (e.target.id == "save") {
-    function result(){
-      let name = document.getElementById('formGroupExampleInput').value;
-      let shortDescription = document.getElementById('formGroupExampleInput2').value;
-      let description = document.getElementById('exampleFormControlTextarea1').value;
+    // function result(){
+    //   let name = document.getElementById('formGroupExampleInput').value;
+    //   let shortDescription = document.getElementById('formGroupExampleInput2').value;
+    //   let description = document.getElementById('exampleFormControlTextarea1').value;
+       window.addEventListener('load',function(){
+        document.querySelector('input[type= "file"]').addEventListener('change', function(){
+          if (this.files && this.files[0]){
+            var img = document.querySelector('img');
+            img.src= URL.createObjectURL(this.files[0]);
+          }
+          
+        });
+      });
+    }
+  // }
 
+    
+        
+   });
+   function imageIsLoaded(){
+    alert(this.src);
+  }
+  imageIsLoaded();
+  result();
+        
+// function readerHandler(e2) 
+// { 
+//   var store = document.getElementById('exampleFormControlFile1');
+//   store.innerHTML=e2.target.result.split("\n").join("<br>"); 
+// }
 
-function readerHandler(e2) 
-{ 
-  var store = document.getElementById('exampleFormControlFile1');
-  store.innerHTML=e2.target.result.split("\n").join("<br>"); 
-}
+// function readfile(e1)
+// {
+//   var fileobj = e1.target.files[0]; 
+//   var fr = new FileReader();
+//   fr.onload = readerHandler;  
+//   fr.readAsText(fileobj); 
+// }
 
-function readfile(e1)
-{
-  var fileobj = e1.target.files[0]; 
-  var fr = new FileReader();
-  fr.onload = readerHandler;  
-  fr.readAsText(fileobj); 
-}
-
-window.onload=function()
-{
-  var x = document.getElementById("filebrowsed");
-  x.addEventListener('change', readfile, false);
-}
+// window.onload=function()
+// {
+//   var x = document.getElementById("filebrowsed");
+//   x.addEventListener('change', readfile, false);
+// }
 
 
 
@@ -99,7 +118,7 @@ window.onload=function()
        
 
       
- }
+ 
  
 //  });
 
